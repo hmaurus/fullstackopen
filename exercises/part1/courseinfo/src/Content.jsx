133c10810f7b0c03aaa80045e416116bc29c33e2
M Henriques <maurus@maurus.com.br>
2025-02-03 00:42:17 -0300

    refactor: 1.3: Course Information step 3

--- exercises/part1/courseinfo/src/Content.jsx
+++ exercises/part1/courseinfo/src/Content.jsx
@@ -1,6 +1,7 @@
 import Part from "./Part";
 
 const Content = (props) => {
+    console.log(props);
     return (
         <div>
             <Part parts={props.parts[0]} exercises={props.exercises[0]} />
