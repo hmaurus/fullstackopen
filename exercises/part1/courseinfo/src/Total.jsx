132a0ee47c1e7619a1fde8a230b853789a1571d3
M Henriques <maurus@maurus.com.br>
2025-02-03 01:00:32 -0300

    refactor: 1.4: Course Information step 4

--- exercises/part1/courseinfo/src/Total.jsx
+++ exercises/part1/courseinfo/src/Total.jsx
@@ -1,6 +1,6 @@
 const Total = (props) => {
     return (
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
     );
   }
   
