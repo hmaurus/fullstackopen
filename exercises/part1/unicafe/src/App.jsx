7472aa2ede9b97c047383e9f25fb29f2183b7410
M Henriques <maurus@maurus.com.br>
2025-02-08 23:49:49 -0300

    chore: 1.6 unicafe step 1

---
+++ exercises/part1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => setGood(good + 1)
+  const handleClickNeutral = () => setNeutral(neutral + 1)
+  const handleClickBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button onClick={handleClickGood} text='good' />
+      <Button onClick={handleClickNeutral} text='neutral' />
+      <Button onClick={handleClickBad} text='bad' />  
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
--- lab1/src/App.jsx
+++ lab1/src/App.jsx
@@ -4,6 +4,8 @@ import Content from "./Content"
 import Total from "./Total"
 import Display from "./Display"
 import Button from "./Button"
+import History from "./History"
+import Button2 from './Button2'
 const App = () => {
   const [counter, setCounter] = useState(0)
   console.log('rendering with counter value', counter)
@@ -40,6 +42,32 @@ const App = () => {
       }
     ]
   }
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  
+  const handleLeftClick = () => {  
+    setAll(allClicks.concat('L'))    
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)  
+    setTotal(updatedLeft + right)
+  }
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R'))  
+    const updatedRight = right + 1  
+    setRight(updatedRight)  
+    setTotal(left + updatedRight)
+  }
+
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
   return (
     <div>
       <Header course={course.name} />
@@ -51,6 +79,23 @@ const App = () => {
       <Button onClick={setToZero} text='zero' />
       <br /><br />
       <Display counter={counter} />
+      <br /><br />
+      <br /><br />
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <br /><br />
+      <History allClicks={allClicks} />
+      <br /><br />
+      <p>Total: {total}</p>
+      <br /><br />
+      <Button2 onClick={() => setToValue(1000)} text='thousand' />
+      <Button2 onClick={() => setToValue(0)} text='reset' />
+      <Button2 onClick={() => setToValue(value + 1)} text='increment' />
+      <br></br>
+      {value}
+      <br></br>
     </div>
   )
 }
