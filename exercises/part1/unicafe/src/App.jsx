a1ddc19806ef7f52b3ad2f83dbdd71d7de46e0ba
M Henriques <maurus@maurus.com.br>
2025-02-09 18:49:44 -0300

    chore: 1.10 unicafe step 5

--- exercises/part1/unicafe/src/App.jsx
+++ exercises/part1/unicafe/src/App.jsx
@@ -17,15 +17,21 @@ const Statistic = ({ good, neutral, bad }) => {
   return (
     <>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
       <p>all {good + neutral + bad}</p>
       <p>average {(good - bad) / (good + neutral + bad)}</p>
       <p>positive {good / (good + neutral + bad) * 100} %</p>
     </>
   )
 }}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
